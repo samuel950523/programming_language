c2bb13f5f06585e324922c39408c75fdfdf0a111
samuel950523 <samuel95@naver.com>
2020-07-06 01:44:23 +0900

    0706

---
+++ Java/JavaEssential/1.element/src/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        // 조건 연산자
+        int a = 3, b = 5;
+
+        System.out.println("두 수의 차는 " + ((a > b) ? (a - b) : (b - a)));
+    }
+}
