69513ed1c3be957d62b660ac2e10e0adf7b90e4a
samuel950523 <samuel95@naver.com>
2020-07-08 06:14:16 +0900

    0708

--- Java/BOJ_CLASS1/1000/src/Main.java
+++ Java/BOJ_CLASS1/1000/src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class sol1000 {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a=scanner.nextInt();
--- Java/BOJ_CLASS1/1001/src/Main.java
+++ Java/BOJ_CLASS1/1001/src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt()-scanner.nextInt());
+        scanner.close();
+    }
 }
