6dc88190660ce8d3faef69590981f1b522bfaa11
samuel950523 <samuel95@naver.com>
2020-09-13 00:30:28 +0900

    0913

---
+++ Java/lectures/0912/src/Solution.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int n;
+	static int[] arr;
+
+	public static void main(String[] args) throws Exception {
+		// 입력
+		br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+	}
+}
